e25b1d5798e7b8f6c0ff394c70aa8ef29dcdfe00
Apple <tiffine.koch@gmail.com>
2016-01-15 12:53:08 -0800

    updated files

--- main.js
+++ main.js
@@ -1,46 +1,128 @@
 $(document).ready(init);
-// $(function init() {
 
-var gameDeck = [];
-var player1 = [];
+var player = [];
 var dealer = [];
-var deckReady = shuffleDeck(gameDeck);
-//object constructor function - make deck
+var gameDeck = [];
+
+
 function init() {
-  //4 of each 9 types
-  var numberArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11","Jack","Queen","King"];
-  //4 of each 1 suit
+//object constructor function - make deck
+  var numberArray = ["1","2", "3", "4", "5", "6", "7", "8", "9", "10","Jack","Queen","King"];
+
   var suitArray = ["heart", "spade", "club", "diamond"];
-  // var deck = (suit.length * numberCard.length);
+
     for(var a = 0; a < suitArray.length; a++) {
       for (var b = 0; b < numberArray.length; b++) {
         gameDeck.push({ "number": numberArray[b], "suit": suitArray[a], "value": b});
-        console.log("hey");
-        console.log("hey");
+
       }
     }
-  shuffleDeck(gameDeck);
-  // dealCards();
-  // $('#start').on('click', dealCards);
-  // $('#hit').on('click', hitClicked);
-  // $('#stand').on('click', standClicked);
+    $('#start').on('click', dealCards);
+    $('#hit').on('click', hitClicked);
+    console.log("cards loaded");
   }
 
-//shufflecards
-function shuffleDeck(gameDeck) {
-    console.log("hey");
-  return _.shuffle(gameDeck);
+    //shufflecards - not working - want to use lodash though
+  // function shuffleDeck(gameDeck) {
+  //     console.log("shuffle");
+  //   return _.shuffle(gameDeck)
+  //   }
+  //
+  // var deckReady = shuffleDeck(gameDeck);
+
+function sendCard() {
+  var $card = $('<img>').addClass("card");
+}
+
+function dealCards(e){
+  $(e.target).off;
+
+  var a = Math.floor(Math.random()*gameDeck.length);
+  var pcards = gameDeck.splice(a, 2);
+  for (var a = 0; a < pcards.length; a++) {
+    player.push(pcards[a]);
   }
 
-// function dealCards(e){
-//   var $target = $(e.target);
-//
+  var b = Math.floor(Math.random()*gameDeck.length);
+  var dcards = gameDeck.splice(b, 2);
+  for (var b = 0; b < dcards.length; b++) {
+    dealer.push(dcards[b]);
+  }
+}
+
+function hitClicked(e) {
+// var $target = $(e.target);
+var pcards = gameDeck.splice(a, 1);
+for (var a = 0; a < pcards.length; a++) {
+  player.push(pcards[a]);
+  }
+}
+
+function standClicked(e) {
+  $(e.target).off;
+  if(playerStand = true) {
+    if(dScore < 17) {
+        dealerPlay();
+    } checkForWinner();
+}
+//dealer rules
+function dealerPlay() {
+  if(dScore < 21) {
+    if(dScore < 17) {
+    var dcards = gameDeck.splice(b, 1);
+    for (var b = 0; a < dcards.length; b++) {
+      player.push(dcards[b]);
+      }
+    } else {
+      checkForWinner();
+    }
+  }
+}
+
+function getpScore() {
+  var pScore = 0;
+  pScore = _.pluck(player, "value");
+
+  pScore.forEach(function(){
+    if (player[0] >= (9)) {
+      pScore += 10;
+    } else {
+      pScore += player[0]+1;
+    }
+  })
+}
+
+function getdScore() {
+  var dScore = 0;
+  dScore = _.pluck(dealer, "value");
+
+  dScore.forEach(function(){
+    if (dealer[0] >= (9)) {
+      dScore += 10;
+    } else {
+      dScore += dealer[0]+1;
+    }
+  })
+}
+
+
+// function getdScore() {
+//   var dScore = _.pluck(player, "value");
+//   return dScore;
 // }
-//
-// function hitClicked(e) {
-//
-// }
-//
-// function standClicked(e) {
-//
-// }
+
+function checkForWinner() {
+  if (pScore > 21) {
+    alert("Player loses")
+  }
+  if (pScore == 21) {
+    alert("Player wins")
+  }
+  if (pScore < 21) {
+    if (pScore > dScore) {
+      alert("Player wins")
+      } else {
+        alert("Player loses")
+    }
+  }
+}
